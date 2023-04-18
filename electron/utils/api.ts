b803224532822ff82fab94d7c3a5067d8913a1e4
liugaowei <liugaowei.sd@chinatelecom.cn>
2023-04-18 13:09:19 +0800

    更新1.9.0

---
+++ electron/utils/api.ts
@@ -0,0 +1,50 @@
+var axios = require("axios");const fs = require("fs");
+
+const speechApi = (ssml: string) => {
+  var data = JSON.stringify({
+    ssml,
+    ttsAudioFormat: "audio-24khz-160kbitrate-mono-mp3",
+    offsetInPlainText: 0,
+    properties: {
+      SpeakTriggerSource: "AccTuningPagePlayButton",
+    },
+  });
+
+  var config = {
+    method: "post",
+    url: "https://southeastasia.api.speech.microsoft.com/accfreetrial/texttospeech/acc/v3.0-beta1/vcg/speak",
+    responseType: "arraybuffer",
+    headers: {
+      authority: "southeastasia.api.speech.microsoft.com",
+      accept: "*/*",
+      "accept-language": "zh-CN,zh;q=0.9",
+      customvoiceconnectionid: "d8a3a480-dd87-11ed-8758-97b5a7fbfaf6",
+      origin: "https://speech.microsoft.com",
+      "sec-ch-ua":
+        '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": '"Windows"',
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-site",
+      "user-agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+      "content-type": "application/json",
+    },
+
+    data: data,
+  };
+
+  return new Promise((resolve, reject) => {
+    axios(config)
+      .then(function (response) {
+        resolve(response.data);
+      })
+      .catch(function (error) {
+        reject(error);
+      });
+  });
+};
+export default {
+  speechApi,
+};
